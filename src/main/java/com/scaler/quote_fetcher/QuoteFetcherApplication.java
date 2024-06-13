4e816b295e07869ddf64bac3e20f5be1202f38ed
Akhila-Kamadi <akhilakamadi@gmail.com>
2024-06-13 15:44:19 +0530

    Project Created

---
+++ src/main/java/com/scaler/quote_fetcher/QuoteFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.quote_fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/quote_fetcher/QuoteFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.quote_fetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
